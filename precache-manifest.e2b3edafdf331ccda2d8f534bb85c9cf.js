d21c9787c84065c514e02326acf68bed697dcb0d
Aditya148 <aditya.gupta0320@gmail.com>
2019-12-01 14:45:57 +0530

    Updates

---
+++ precache-manifest.e2b3edafdf331ccda2d8f534bb85c9cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2d1f5b046d1f51c703c06d440d09bae",
+    "url": "/uNick/index.html"
+  },
+  {
+    "revision": "2c396c633914a3dcab98",
+    "url": "/uNick/static/css/main.0c906593.chunk.css"
+  },
+  {
+    "revision": "7ef42a35a4b773625d0c",
+    "url": "/uNick/static/js/2.f9e1a0bf.chunk.js"
+  },
+  {
+    "revision": "2c396c633914a3dcab98",
+    "url": "/uNick/static/js/main.3386d74a.chunk.js"
+  },
+  {
+    "revision": "9a5a051422762b2005ef",
+    "url": "/uNick/static/js/runtime-main.e572e11f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/uNick/static/media/logo.25bf045c.svg"
+  }
+]);
